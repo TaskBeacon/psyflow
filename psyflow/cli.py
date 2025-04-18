7c57422cb974e05366499c8eeb9724dcaee8ac1c
zh1peng <frueddy@gmail.com>
2025-04-18 13:37:30 +0800

    add cookecuter

---
+++ psyflow/cli.py
@@ -0,0 +1,17 @@
+import click
+from cookiecutter.main import cookiecutter
+import importlib.resources as pkg_res
+
+@click.command()
+@click.argument("project_name")
+def main(project_name):
+    # pkg_res.files("psyflow.templates") points at the 'templates/' package folder
+    tmpl_dir = pkg_res.files("psyflow.templates") / "cookiecutter-psyflow"
+    cookiecutter(
+        str(tmpl_dir),
+        no_input=True,
+        extra_context={"project_name": project_name}
+    )
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -3,10 +3,25 @@ from setuptools import setup, find_packages
 setup(
     name="psyflow",
     version="0.1.0",
+    description="A utility package for building modular PsychoPy experiments.",
+    author="Zhipeng Cao",
+    author_email="zhipeng30@foxmail.com",
     packages=find_packages(),
     install_requires=[
         "psychopy",
         "numpy",
-        "pandas"
+        "pandas",
+        "click",           # for CLI support
+        "cookiecutter",    # for template-based scaffolding
     ],
+    entry_points={
+        "console_scripts": [
+            "psyflow-init = psyflow.cli:main",
+        ]
+    },
+    include_package_data=True,
+    package_data={
+        "psyflow": ["templates/cookiecutter-psyflow/**/*"],
+    },
+    zip_safe=False
 )
