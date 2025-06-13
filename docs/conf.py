069d7e5caf7e58df647ff3b7bc44be0aa49f7315
zh1peng <frueddy@gmail.com>
2025-06-13 09:02:28 +0800

    update doc settings

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
 # Enable extensions
 extensions = [
     "myst_parser",                      # Markdown support
